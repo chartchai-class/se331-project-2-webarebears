dc2b9c8778a9d9d6ea302442ff32e83a04cde21d
matchima <mkhamyod@gmail.com>
2024-10-25 10:38:41 +0700

    update router

--- se331-project-2-webarebears/src/router/index.ts
+++ se331-project-2-webarebears/src/router/index.ts
@@ -1,23 +1,114 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import EventListView from '@/views/ListView.vue'
+import LayoutView from '@/views/event/LayoutView.vue'
+import CountryDetail from '@/views/event/CountryDetailView.vue'
+import MedalDetail from '@/views/event/MedalView.vue'
+import NotFoundView from '@/views/NotFoundView.vue'
+import NetworkErrorView from '@/views/NetworkErrorView.vue'
+import Login from '@/views/Login.vue'
+import Signup from '@/views/Signup.vue'
+import Comment from '@/views/event/CommentView.vue'
+import nProgress from 'nprogress'
+import EventService from '@/services/Service'
+import { useEventStore } from '@/stores/event'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView
+      name: 'list-view',
+      component: EventListView,
+      props: (route) => ({ page: parseInt(route.query.page?.toString() || '1') })
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      path: '/country/:id',
+      name: 'layout-view',
+      component: LayoutView,
+      props: true,
+      beforeEnter: async (to, from, next) => {
+        const eventStore = useEventStore()
+        const id = to.params.id as string
+        try {
+          const event = await EventService.getEvent(id)
+          if (event) {
+            eventStore.setEvent(event)
+            next()
+          } else {
+            next({ name: '404-resource-view', params: { resource: 'country' } })
+          }
+        } catch (error) {
+          console.error('Error fetching country data:', error)
+          next({ name: 'network-error-view', params: { resource: 'page' } })
+        }
+      },
+      children: [
+        {
+          path: 'details',
+          name: 'country-detail-view',
+          component: CountryDetail,
+          props: true
+        },
+        {
+          path: 'medals',
+          name: 'medal-detail-view',
+          component: MedalDetail,
+          props: true
+        },
+        {
+          path: 'comment',
+          name: 'comment-view',
+          component: Comment,
+          props: true
+        }
+
+      ]
+    },
+    {
+      path: '/404/:resource',
+      name: '404-resource-view',
+      component: NotFoundView,
+      props: true
+    },
+    {
+      path: '/:catchAll(.*)',
+      name: 'not-found',
+      component: NotFoundView
+    },
+    {
+      path: '/network-error',
+      name: 'network-error-view',
+      component: NetworkErrorView
+    },{
+      path:'/login',
+      name: 'login',
+      component:Login
     }
-  ]
+    ,{
+      path:'/signup',
+      name: 'signup',
+      component:Signup
+    }
+  
+
+  ],
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition
+    } else {
+      return { top: 0 }
+    }
+  }
 })
+
+router.beforeEach(() => {
+  nProgress.start()
+})
+
+router.afterEach(() => {
+  nProgress.done()
+})
+
+
 
 export default router
