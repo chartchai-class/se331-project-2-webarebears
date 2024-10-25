ce80088389bb1f5f2259bcaeab516a328327bff3
matchima <mkhamyod@gmail.com>
2024-10-25 18:01:47 +0700

    update tailwind
    add new color

--- se331-project-2-webarebears/tailwind.config.ts
+++ se331-project-2-webarebears/tailwind.config.ts
@@ -2,28 +2,32 @@
 import type { Config } from 'tailwindcss'
 
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}"
-  ],
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
     extend: {
-      boxShadow:{
-        'sp': '0 3px 12px 0 rgba(0, 0, 0, 0.2)'
+      boxShadow: {
+        sp: '0 3px 12px 0 rgba(0, 0, 0, 0.2)',
       },
       fontFamily: {
         sans: ['Avenir', 'Helvetica', 'Arial', 'sans-serif'],
       },
-      animation:{
+      animation: {
         fade: 'yellowFade 3s ease-in-out',
       },
-      keyframes:{
+      keyframes: {
         yellowFade: {
           '0%': { backgroundColor: 'yellow' },
           '100%': { backgroundColor: 'transparent' },
-        }
-      }
-    }
+        },
+      },
+      colors: {
+        customRed: '#AB3428',
+        customOrange: '#F49E4C',
+        customBlue:'#3B8EA5',
+        customYellow:'#f5ee9e'
+
+      },
+    },
   },
   plugins: [],
 } satisfies Config
