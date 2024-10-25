ea03aa317b0d04e19f438dac6c5b208d8f47e8e5
matchima <mkhamyod@gmail.com>
2024-10-25 10:43:05 +0700

    update main.ts and add type.ts

--- se331-project-2-webarebears/src/main.ts
+++ se331-project-2-webarebears/src/main.ts
@@ -1,8 +1,8 @@
-import './assets/main.css'
-
+// import './assets/main.css'
+import './assets/style.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
+import 'nprogress/nprogress.css'
 import App from './App.vue'
 import router from './router'
 
